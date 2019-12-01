8b0b8d8d2eee062f5c0212dc70c645edfa6fe270
Bogdan <bodya.bodnarenko@gmail.com>
2019-12-01 17:17:18 +0200

    added stars

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from "react";
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 import { Editor, shapes } from "./editor/editor";
+import { Renderer } from "./utils/renderer";
 
 const randomFromRange = (min, max) => Math.random() * (max - min) + min;
 const shapeNames = Object.keys(shapes);
@@ -9,65 +9,15 @@ const shapeNames = Object.keys(shapes);
 class App extends Component {
   componentDidMount() {
     this.shapes = {};
-    this.sceneSetup();
-    this.startAnimationLoop();
-    window.addEventListener("resize", this.handleWindowResize);
-  }
-
-  constructor() {
-    super();
-    this.editor = new Editor();
+    this.renderer = new Renderer(this.el);
+    this.scene = this.renderer.scene;
+    this.editor = new Editor(this.scene);
+    this.animate();
   }
 
   componentWillUnmount() {
-    window.removeEventListener("resize", this.handleWindowResize);
-    window.cancelAnimationFrame(this.requestID);
-    this.controls.dispose();
+    this.renderer.unmount(this.requestID);
   }
-
-  sceneSetup = () => {
-    const width = this.el.clientWidth;
-    const height = this.el.clientHeight;
-
-    this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0xe1e1e1);
-    this.scene.name = "Scene";
-
-    const ambientLight = new THREE.AmbientLight(0xcccccc, 1);
-    ambientLight.name = "AmbientLight";
-    this.scene.add(ambientLight);
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4);
-    directionalLight.name = "DirectionalLight";
-    directionalLight.position.set(1, 1, 1).multiplyScalar(10);
-    this.scene.add(directionalLight);
-
-    const pointLight = new THREE.PointLight(0xffffff, 0.4);
-    pointLight.name = "PointLight";
-    pointLight.position.set(0, 200, 0);
-    this.scene.add(pointLight);
-
-    this.camera = new THREE.PerspectiveCamera(
-      75, // fov = field of view
-      width / height, // aspect ratio
-      0.1, // near plane
-      1000 // far plane
-    );
-    this.camera.position.z = 5; // is used here to set some distance from a cube that is located at z = 0
-    // OrbitControls allow a camera to orbit around the object
-    // https://threejs.org/docs/#examples/controls/OrbitControls
-    this.controls = new OrbitControls(this.camera, this.el);
-
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize(width, height);
-
-    window.THREE = THREE;
-    window.controls = this.controls;
-    window.scene = this.scene;
-    window.renderer = this.renderer;
-
-    this.el.appendChild(this.renderer.domElement); // mount using React ref
-  };
 
   addRandomMesh = () => {
     const shapeIndex = Math.round(randomFromRange(0, shapeNames.length - 1)),
@@ -76,34 +26,18 @@ class App extends Component {
         y: randomFromRange(-3, 3),
         z: randomFromRange(-1, 2)
       };
-    const shape = this.editor.addShape(
-      shapeNames[shapeIndex],
-      this.scene,
-      shapePosition
-    );
+    const shape = this.editor.addShape(shapeNames[shapeIndex], shapePosition);
     this.shapes[shape.id] = shape;
   };
 
-  startAnimationLoop = () => {
+  animate = () => {
     for (const { mesh } of Object.values(this.shapes)) {
       mesh.rotation.x += 0.01;
       mesh.rotation.y += 0.01;
     }
 
-    this.renderer.render(this.scene, this.camera);
-    this.controls.update();
-
-    this.requestID = window.requestAnimationFrame(this.startAnimationLoop);
-  };
-
-  handleWindowResize = () => {
-    const width = this.el.clientWidth;
-    const height = this.el.clientHeight;
-
-    this.renderer.setSize(width, height);
-    this.camera.aspect = width / height;
-
-    this.camera.updateProjectionMatrix();
+    this.renderer.render();
+    this.requestID = window.requestAnimationFrame(this.animate);
   };
 
   render() {
--- src/editor/editor.js
+++ src/editor/editor.js
@@ -2,11 +2,16 @@ import * as shapeTypes from "./shapes/";
 
 export const shapes = shapeTypes;
 export class Editor {
-  addShape(type, scene, position = { x: 0, y: 0, z: 0 }) {
+  constructor(scene) {
+    this.scene = scene;
+  }
+
+  addShape(type, position = { x: 0, y: 0, z: 0 }) {
     if (!shapes[type]) {
       console.warn(`Shape with type "${type}" does not exist`);
       return null;
     }
+
     const shape = new shapes[type](type, false);
 
     if (!position.x || !position.y || !position.y) {
@@ -15,7 +20,7 @@ export class Editor {
       shape.position = position;
     }
 
-    shape.addToScene(scene);
+    shape.addToScene(this.scene);
 
     return shape;
   }
